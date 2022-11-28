2f11792e52d021887cd9e46a4b3de07d7a63e11b
TASTYBURGER97 <tihonov.artyom1997@yandex.ru>
2022-11-29 01:20:34 +0400

    ДЗ готово

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //Task1
+        System.out.println("//Task1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        //Task2
+        System.out.println("//Task2");
+        for (int i = 10; i >= 1; i--) {
+
+            System.out.println(i);
+
+        }
+        //Task3
+        System.out.println("//Task3");
+        for (int i = 0; i < 17; i = i + 2) {
+            System.out.println(i);
+        }
+
+
+
+        //Task4
+        System.out.println("//Task4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+
+
